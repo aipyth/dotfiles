996994d4f1e30d075760a8f5fec0ab7e4d5a80c2
Ivan <aipyth.func@gmail.com>
2022-03-05 17:14:56 +0200

    alalla

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+killall polybar
+<<<<<<< HEAD
+polybar example &
+polybar example2 &
+polybar now-playing &
+=======
+polybar bspwm &
+polybar music &
+polybar other &
+>>>>>>> ac15a06 (a)
